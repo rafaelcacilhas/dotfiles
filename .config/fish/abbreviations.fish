2ef6653bfff8b283fbfefab95481370dee5be0d0
Dilip Chauhan <chauhandilip305@gmail.com>
2022-09-11 16:39:50 +0530

    Updated on 11 September 2022

---
+++ .config/fish/abbreviations.fish
@@ -0,0 +1,21 @@
+abbr config 'git --git-dir=$HOME/dotfiles --work-tree=$HOME'
+
+abbr :q 'exit'
+abbr :wq 'exit'
+abbr :x 'exit'
+
+abbr cp 'cp -i'
+abbr rm 'rm -i'
+abbr mv 'mv -i'
+abbr exa "exa --color=always --icons"
+abbr l "exa --color=always --icons"
+abbr ls "ls --color=auto"
+abbr la 'exa -a --color=always --icons'
+abbr ll 'exa -l --color=always -h --icons --tree --level=1'
+abbr all 'exa -al --color=always -h --icons --tree --level=1'
+abbr cat "batcat"
+abbr n "nvim"
+abbr clear "clear && fm6000 -c blue"
+abbr install "sudo apt install"
+abbr update "sudo apt update"
+abbr upgrade "sudo apt upgrade"
---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+set --export SHELL /usr/bin/fish
+set --export EDITOR "nvim"
+set -e fish_greeting
+
+
+source "$HOME/.config/fish/abbreviations.fish"
