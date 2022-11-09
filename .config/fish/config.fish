49ac42c6ee904161b0bbb8fdf98faec2b8cd0c1f
Rafael Cacilhas <rafaelcacilhas@gmail.com>
2022-11-09 14:51:52 -0300

    first version

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-set --export SHELL /usr/bin/fish
-set --export EDITOR "nvim"
-set --export PATH "$PATH $HOME/.scripts/"
-set fish_greeting ""
-
-fish_vi_key_bindings
-
-source "$HOME/.config/fish/abbreviations.fish"
